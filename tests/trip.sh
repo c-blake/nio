244043843e951a336817f3f078e8ed9e9d0b4724
Charles Blake <charlechaud@gmail.com>
2021-05-21 10:47:16 -0400

    Add a bunch of nio cut tests; identify at least one bug to fix..

--- tests/trip.sh
+++ tests/trip.sh
@@ -113,3 +113,17 @@ nio f -srip.schema "" < data    # empty string another way to indicate stdin
 mv Ign.Ni Ign
 echo i@%s > .Ign
 nio p -a rstrings.LS Ign
+
+nio z chF2.Ni4c Date.Ni > f3.Ni4Ci
+nio c -d 0 f3.Ni4Ci | nio p .N4Ci
+nio c -d 1 f3.Ni4Ci | nio p .Nii
+nio c -d 2 f3.Ni4Ci | nio p .Ni4C
+nio c -d 0:3 f3.Ni4Ci | nio p .Ni # no rows so fmt doesn't matter
+nio c -d 0:2 f3.Ni4Ci | nio p .Ni
+nio c -d 1:3 f3.Ni4Ci | nio p .Ni
+nio c -d :1  f3.Ni4Ci | nio p .N4Ci
+nio c -d :-1 f3.Ni4Ci | nio p .Ni
+nio c -d 1:-1 f3.Ni4Ci | nio p .Nii
+nio c -d 2:-1 f3.Ni4Ci | nio p .Ni4Ci
+#nio c -d 1:   f3.Ni4Ci | nio p .Ni  # XXX broken
+#nio c -d 0:   f3.Ni4Ci | nio p .Ni  # XXX broken
