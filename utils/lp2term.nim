6fc3470d85c830c2fa40e4252d9ea7082a9e6cab
Charles Blake <charlechaud@gmail.com>
2021-05-23 07:34:23 -0400

    Add a tiny little converter/printer/dumper utility for LP files.

---
+++ utils/lp2term.nim
@@ -0,0 +1,25 @@
+import nio, cligen/osUt, cligen
+
+proc lp2term*(eor='\0', subEor=" ", index="", paths: Strings) =
+  ## Concatenate len-prefixed buf repos `paths` to stdout as `eor`-terminated
+  ## with optional index creation.
+  var ix: NFile
+  var off: int; var obuf: array[8, char]
+  if index.len > 0:
+    ix = nOpen(index, fmWrite)
+  for path in paths:
+    var r = rOpen(path)
+    for k, i in r.keysAtOpen:
+      if ix.f != nil: #XXX replace below mess with nice `ix.write off` API
+        convert(ix.rowFmt.cols[0].iok, lik, obuf[0].addr, off.addr)
+        ix.f.nurite ix.rowFmt.cols[0].iok, obuf[0].addr
+      off.inc k.len + 1
+      stdout.replacingUrite k, eor, subEor
+      stdout.urite eor
+    r.close
+
+when isMainModule:
+  dispatch lp2term, help={"paths" : "{1|more paths to a .L file}",
+                          "eor"   : "output record terminator",
+                          "subEor": "string for rec term inside bufs",
+                          "index" : "optional binary start-of-buf NIO file"}
