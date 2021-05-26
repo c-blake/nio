8410ff28107ee40fc57343584f1d523a3c47872d
Charles Blake <charlechaud@gmail.com>
2021-05-26 08:43:42 -0400

    Cross check mmap & streaming.

--- tests/06-tails.sh
+++ tests/06-tails.sh
@@ -1,27 +1,31 @@
-#!/bin/sh -ex
+#!/bin/sh -e
 . ${0%06-tails.sh}setup.sh
 t=09
 seq 0 9 | nio l -id -oi > $t.Ni
 ns="0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 6 17 18 19"
 
-# first test mman/rand access impls
-for extra in "" "-c" "-r" "-rc"; do
+# first test mmap/rand access impls
+(for extra in "" "-c" "-r" "-rc"; do
   for n in $ns; do
-    nio t -h$n $t.Ni | nio p .Ni
-    nio t -t$n $t.Ni | nio p .Ni
+    nio t $extra -h$n $t.Ni | nio p .Ni
+    nio t $extra -t$n $t.Ni | nio p .Ni
     for m in $ns; do
-      nio t -h$m -t$n $t.Ni | nio p .Ni
+      nio t $extra -h$m -t$n $t.Ni | nio p .Ni
     done
   done
-done
+done) > tailsMap
 
 # then streaming impls
-for extra in "" "-c" "-r" "-rc"; do
+(for extra in "" "-c" "-r" "-rc"; do
   for n in $ns; do
-    nio t -h$n $t.Ni | nio p .Ni
-    nio t -t$n $t.Ni | nio p .Ni
+    nio t $extra -h$n $t.Ni | nio p .Ni
+    nio t $extra -t$n $t.Ni | nio p .Ni
     for m in $ns; do
-      cat $t.Ni | nio t -h$m -t$n .Ni | nio p .Ni
+      cat $t.Ni | nio t $extra -h$m -t$n .Ni | nio p .Ni
     done
   done
-done
+done) > tailsStm
+
+# Consider the impls of mmap/one-pass streaming distinct enough to be a
+# good cross-check.
+diff tailsMap tailsStm
