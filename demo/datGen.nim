e10c994787f9601caa64fdded6ce8edec3131ac9
Charles Blake <charlechaud@gmail.com>
2021-08-20 11:55:30 -0400

    Add ability for datGen to also generate integer data with whatever mean and standard deviation you want as a way to test group label operations.  Also add a simple shell script `group.sh` to show how to use it/also test it.

--- demo/datGen.nim
+++ demo/datGen.nim
@@ -1,7 +1,9 @@
 import std/[os, strutils, random]
 # randomize()
-let n = if paramCount() > 0: parseInt(paramStr(1)) else: 30
-let m = if paramCount() > 1: parseInt(paramStr(2)) else: 10
+let n    = if paramCount() > 0: parseInt(paramStr(1)) else: 30
+let m    = if paramCount() > 1: parseInt(paramStr(2)) else: 10
+let mean = if paramCount() > 2: parseInt(paramStr(3)) else: 0
+let sdev = if paramCount() > 2: parseInt(paramStr(3)) else: 1
 
 let cols = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m",
             "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",
@@ -12,11 +14,15 @@ if m > cols.len: stderr.write "too many cols\n"; quit(1)
 var base = ""; var fmt = ""     # create a "descriptive" pathname
 for j in 0..<m:
   base.add cols[j]
-  fmt.add 'f'
+  fmt.add if mean > 0: 'l' else: 'f'
 
 var f = open(base & ".N" & fmt, fmWrite)
 for i in 0 ..< n:               # write data to data file
   for j in 0 ..< m:
     let r = float32(randState.gauss(j.float, float(j + 1)))
-    discard f.writeBuffer(r.unsafeAddr, r.sizeof)
+    if mean > 0:
+      let k = int(max(0, mean.float + r * sdev.float))
+      discard f.writeBuffer(k.unsafeAddr, k.sizeof)
+    else:
+      discard f.writeBuffer(r.unsafeAddr, r.sizeof)
 close f
---
+++ demo/group.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+N=30
+if [ $# -gt 0 ]; then
+    N=$1
+    shift 1
+fi
+datGen $N 3     # generate 3 columns of data
+datGen $N 1 3   # generate some group labels
+
+# Massage a little into a multi-file/multi-column test case
+nio rip -i abc.Nfff x.Nf y.Nf z.Nf
+nio zip x.Nf y.Nf > xy.Nff
+rm abc.Nfff x.Nf y.Nf
