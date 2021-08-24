9472f1f26a27681bb370a55efe451384af652bc4
Charles Blake <charlechaud@gmail.com>
2021-08-24 14:03:52 -0400

    Simplify a bit more with `math.sum`.

--- demo/favg.nim
+++ demo/favg.nim
@@ -1,15 +1,13 @@
 {.passC: "-O3 -ffast-math -march=native -mtune=native".}
-import nio, strutils, cligen
+import strutils, math, cligen, nio
 
 proc favg(fmt=".5g", n=100, paths: seq[string]): int =
   ## print averages of `paths` **which must be float32 column files**
   for path in paths:
     var inp = initFileArray[float32](path)
     var tot = 0.0'f32 # 64 precise, but 32 allows vectorization
-    for count in 1..n:
-      for f in inp:   # gcc requires -ffast-math to use a..
-        tot = tot + f #.."matrix" accumulation vaddps here.
-    echo path, " ", formatFloat(tot / inp.len.float, fmt)
+    for count in 1..n: tot = tot + inp.toOpenArray.sum
+    echo path, " ", formatFloat(tot/inp.len.float/n.float, fmt)
     inp.close
 
 dispatch favg
