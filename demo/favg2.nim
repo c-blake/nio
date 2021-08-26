6d2528848ff899feb99d34b7642093545c99321b
Charles Blake <charlechaud@gmail.com>
2021-08-26 11:29:45 -0400

    Better comments.

--- demo/favg2.nim
+++ demo/favg2.nim
@@ -6,11 +6,11 @@ import strutils, math, cligen, nio
 proc favg(fmt=".5g", n=100, paths: seq[string]): int =
   ## print averages of `paths` **which must be float32 column files** using
   ## non-mmap IO and a GC'd IO buffer.  About 2X slower for me on Linux.
-  var buf: seq[float32] # re-use buffer over `paths`
+  var buf: seq[float32]         # re-use buffer over `paths`
   for path in paths:
-    buf.setLen 0        # but trunc before adding
-    buf.add path, naCvt=true
-    var tot = 0.0'f32   # 64 precise, but 32 allows vectorization
+    buf.setLen 0                # but trunc before adding
+    buf.add path, naCvt=true    # read binary file and add to `buf`
+    var tot = 0.0'f32           # 64 precise, but 32 enhances vectorization
     for count in 1..n: tot = tot + buf.sum
     echo path, " ", formatFloat(tot/buf.len.float/n.float, fmt)
 
