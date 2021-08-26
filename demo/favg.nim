37eae63b6be8922f1b11e54c6740543bcad4ad1e
Charles Blake <charlechaud@gmail.com>
2021-08-26 11:49:08 -0400

    Better comment.

--- demo/favg.nim
+++ demo/favg.nim
@@ -7,7 +7,7 @@ proc favg(fmt=".5g", n=100, paths: seq[string]): int =
   ## print averages of `paths` **which must be float32 column files**
   for path in paths:
     var inp = initFileArray[float32](path)  # could be `load`
-    var tot = 0.0'f32 # 64 precise, but 32 allows vectorization
+    var tot = 0.0'f32 # 64 precise, but 32 enhances vectorization
     for count in 1..n: tot = tot + inp.toOpenArray.sum  # or `toOA`
     echo path, " ", formatFloat(tot/inp.len.float/n.float, fmt)
     inp.close
